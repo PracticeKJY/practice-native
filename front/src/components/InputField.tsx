530a8da41c1a0101ab623a4c04c932b0a077aaa1
codespace-jake <119389577+PracticeKJY@users.noreply.github.com>
2025-03-27 16:29:20 +0900

    feat. input 컴포넌트 제작 및 유효성,검증 및 폼 상태 커스텀 훅 제작

---
+++ front/src/components/InputField.tsx
@@ -0,0 +1,90 @@
+import React, {useRef} from 'react';
+import {
+  Dimensions,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  TextInputProps,
+  View,
+} from 'react-native';
+import {colors} from '../constants';
+
+interface InputFieldProps extends TextInputProps {
+  placeHolder: string;
+  disabled?: boolean;
+  error?: string;
+  touched?: boolean;
+}
+
+const deviceHeight = Dimensions.get('screen').height;
+
+// 자동대문자방지 = autoCapitalize<"none" | "sentences" | "words" | "characters" | undefined>
+// 자동완성활성화 = autoCorrect<boolean | undefined>
+// 맞춤법검사기능 = spellCheck<boolean | undefined>
+const InputField = ({
+  disabled = false,
+  placeHolder,
+  error,
+  touched,
+  ...props
+}: InputFieldProps) => {
+  const innerRef = useRef<TextInput | null>(null);
+  // input 전체를 Pressable로 만들고 어딜 클릭하던 focus 되도록 설정함.
+  const handlePressInput = () => {
+    innerRef.current?.focus();
+  };
+
+  return (
+    <Pressable onPress={handlePressInput}>
+      <View
+        style={[
+          styles.container,
+          disabled && styles.disabled,
+          touched && Boolean(error) && styles.inputError,
+        ]}>
+        <TextInput
+          ref={innerRef}
+          editable={!disabled}
+          placeholder={placeHolder}
+          placeholderTextColor={colors.light.GRAY_500}
+          style={[styles.input, disabled && styles.disabled]}
+          autoCapitalize="none"
+          spellCheck={false}
+          autoCorrect={false}
+          {...props}
+        />
+        {/* Boolean(error) => 빈문자열이 아닌 경우 에러메세지 */}
+        {touched && Boolean(error) && <Text style={styles.error}>{error}</Text>}
+      </View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderColor: colors.light.GRAY_200,
+    padding: deviceHeight > 700 ? 15 : 10,
+  },
+  disabled: {
+    backgroundColor: colors.light.GRAY_200,
+    color: colors.light.GRAY_700,
+  },
+  input: {
+    fontSize: 16,
+    color: colors.light.BLACK,
+    padding: 0,
+  },
+  inputError: {
+    borderWidth: 1,
+    borderColor: colors.light.RED_300,
+  },
+  error: {
+    fontSize: 12,
+    padidngTop: 5,
+    color: colors.light.RED_500,
+  },
+});
+
+export default InputField;
--- front/src/screens/auth/LoginScreen.tsx
+++ front/src/screens/auth/LoginScreen.tsx
@@ -1,14 +1,64 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+import InputField from '../../components/InputField';
+import CustomButton from '../../components/CustomButton';
+import useForm from '../../hooks/useForm';
+import {validateLogin} from '../../utils';
+
+// Input props
+// ****  = secureTextEntry
+// inputMode< 'none' |'text'|'decimal'|'numeric'|'tel'|'search'|'email'|'url';> = 다양한 키보드 형태
 
 const LoginScreen = () => {
+  const login = useForm({
+    initialValue: {
+      email: '',
+      password: '',
+    },
+    validate: validateLogin,
+  });
+
+  const handleSubmit = () => {
+    console.log(login.values, 'values');
+  };
+
   return (
-    <View>
-      <Text>로그인 스크린</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField
+          placeHolder="이메일"
+          error={login.errors.email}
+          inputMode="email"
+          touched={login.touched.email}
+          {...login.getTextInputProps('email')}
+        />
+        <InputField
+          placeHolder="비밀번호"
+          error={login.errors.password}
+          touched={login.touched.password}
+          {...login.getTextInputProps('password')}
+          secureTextEntry
+        />
+      </View>
+      <CustomButton
+        label="로그인"
+        variant="filled"
+        size="large"
+        onPress={handleSubmit}
+      />
+    </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+    marginBottom: 30,
+  },
+});
 
 export default LoginScreen;
